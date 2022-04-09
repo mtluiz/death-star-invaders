8017c6da86092918ccc302378735399cd3784db6
Daniel <daniel.fujita@gmail.com>
2022-04-08 23:23:05 -0300

    teste

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Screen from './Screen'
 import { createRoot } from 'react-dom/client'
 import './Screen.css'
-
+const macaco = "a"
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
