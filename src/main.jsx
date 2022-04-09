5041afc527652f67e95aee8e4c4196899a8393c9
Matheus Madureira <matheus.madureira@g.globo>
2022-04-09 06:28:16 -0300

    chore: using react 18 createRoot

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import Screen from './Screen'
-import { createRoot } from 'react-dom/client'
-import './Screen.css'
+import React from "react";
+import Screen from "./Screen";
+import { createRoot } from "react-dom/client";
+import "./Screen.css";
 
-
-const rootElement = document.getElementById('root')
-const root = createRoot(rootElement)
-root.render(<div className='Screen'><Screen /></div>)
-
-
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+  <div className="Screen">
+      <Screen />
+  </div>
+);
