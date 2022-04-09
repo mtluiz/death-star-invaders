5ac191cee9f8b0901e1ed83f4dd8a1a9a3a54908
Matheus Madureira <matheus.madureira@g.globo>
2022-04-09 18:54:24 -0300

    feat: creating player controls

--- src/components/GameScene.jsx
+++ src/components/GameScene.jsx
@@ -1,34 +1,56 @@
-import { useThree } from '@react-three/fiber';
-import { useGameContext } from '../context/gameContext';
-import Box from '../objects/Box'
+import { useThree, useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useGameContext } from "../context/gameContext";
+import { Suspense, useRef } from "react";
+import Box from "../objects/Box";
 
 const GameScene = () => {
+  const { playerPosition, setPlayerPosition } = useGameContext();
 
-  const {playerPosition, setPlayerPosition} = useGameContext()
-  
-  useThree(({camera}) => {
-    console.log(camera)
-  })
+  const tie = useLoader(
+    GLTFLoader,
+    "./src/assets/3d-models/tie_interceptor/scene.gltf"
+  );
 
-  window.addEventListener('keypress', (e) => {
-    if (e.key == 'a') {
-      if(playerPosition.x > -6) setPlayerPosition({x: playerPosition.x - 1})
+  const millenium = useLoader(
+    GLTFLoader,
+    "./src/assets/3d-models/millenium-falcom/scene.gltf"
+  );
+
+  useThree(({ camera }) => {
+    camera.position.z = 10;
+  });
+
+  window.addEventListener("keypress", (e) => {
+    if (e.key == "a") {
+      if (playerPosition.x > -10)
+        setPlayerPosition({ x: playerPosition.x - 1 });
     }
-  })
+  });
 
-
-  window.addEventListener('keypress', (e) => {
-    if (e.key == 'd') {
-      if(playerPosition.x < 6) setPlayerPosition({x: playerPosition.x + 1})
+  window.addEventListener("keypress", (e) => {
+    if (e.key == "d") {
+      if (playerPosition.x < 10) setPlayerPosition({ x: playerPosition.x + 1 });
     }
-  })
-
+  });
 
   return (
-        <Box playerPosition={playerPosition}/>
-  )
-}
+    <>
+      <ambientLight color={0xaaaaaa} />
+      <Box playerPosition={playerPosition} />
+
+      <gridHelper args={[10, 10, 10]} />
+
+      <Suspense fallback={null}>
+        <primitive
+          object={tie.scene}
+          scale={0.4}
+          position={[-6, 2, -3]}
+          rotation={[0.1, 0.6, 0]}
+        />
+      </Suspense>
+    </>
+  );
+};
 
 export default GameScene;
-
-
