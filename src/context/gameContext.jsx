e9fdb8f2d278d869765514019fc1e7cdf946ebcb
Matheus Madureira <matheus.madureira@g.globo>
2022-04-09 06:25:37 -0300

    feat: adding game context

--- src/context/gameContext.jsx
+++ src/context/gameContext.jsx
@@ -1,14 +1,31 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useContext } from "react";
 
-const GameContext = createContext({});
+const GameContext = createContext();
 
-const GameContextProvider = (children) => {
+const GameContextProvider = ({children}) => {
+
   const [playerPosition, setPlayerPosition] = useState({ x: 0 });
 
+  const value = {
+    playerPosition,
+    setPlayerPosition
+  }
+
   return (
-    <GameContext.Provider value={{ playerPosition, setPlayerPosition }}>
+    <GameContext.Provider value={value}>
       {children}
     </GameContext.Provider>
   );
 };
 
+const useGameContext = () => {
+  const context = useContext(GameContext);
+
+  if (!context) {
+    console.log('me de contexto')
+  }
+
+  return context
+};
+
+export { GameContextProvider, useGameContext, GameContext };
