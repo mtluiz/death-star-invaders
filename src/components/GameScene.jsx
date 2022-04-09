3e4c01a07f151629d8b0c0ddffc26e54998521be
Matheus Madureira <matheus.madureira@g.globo>
2022-04-09 06:27:30 -0300

    feat: experimenting on player controls

--- src/components/GameScene.jsx
+++ src/components/GameScene.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import { useThree } from '@react-three/fiber';
+import { useGameContext } from '../context/gameContext';
 import Box from '../objects/Box'
 
-export default function GameScene() {
+const GameScene = () => {
+
+  const {playerPosition, setPlayerPosition} = useGameContext()
+  
+  useThree(({camera}) => {
+    console.log(camera)
+  })
 
   window.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-      console.log('aperto enter')
+    if (e.key == 'a') {
+      if(playerPosition.x > -6) setPlayerPosition({x: playerPosition.x - 1})
     }
   })
 
+
+  window.addEventListener('keypress', (e) => {
+    if (e.key == 'd') {
+      if(playerPosition.x < 6) setPlayerPosition({x: playerPosition.x + 1})
+    }
+  })
+
+
   return (
-    <>
-        <Box onKeyPress={()=>{console.log('a')}} />
-    </>
+        <Box playerPosition={playerPosition}/>
   )
 }
+
+export default GameScene;
 
 
