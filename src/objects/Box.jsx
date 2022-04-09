c44302ce4ee5e2be5888be67e9729de757600764
Matheus Madureira <matheus.madureira@g.globo>
2022-04-09 06:27:49 -0300

    feat: creating box object for tests

--- src/objects/Box.jsx
+++ src/objects/Box.jsx
@@ -1,15 +1,17 @@
 import * as THREE from "three";
 import {useFrame} from "@react-three/fiber";
 import { useRef } from "react";
+import { useGameContext } from "../context/gameContext";
 
 export default function Box(props) {
+  const teste = useGameContext()
+  console.log(props.playerPosition.x) 
   const ref = useRef(null);
-
-  
 
   useFrame((state, delta) => {
     ref.current.rotation.x += 0.1;
     ref.current.position.y = -3;
+    ref.current.position.x = props.playerPosition.x;
   });
   return (
     <mesh {...props} ref={ref}>
