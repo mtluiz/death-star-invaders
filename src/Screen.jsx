e9baa94b927064e9f7d6a81315b23cae4ab4c37e
Matheus Madureira <matheus.madureira@g.globo>
2022-04-10 01:56:50 -0300

    feat: making sound tests

--- src/Screen.jsx
+++ src/Screen.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { GameContextProvider, useGameContext } from "./context/gameContext";
 import { Canvas } from "@react-three/fiber";
 import GameScene from "./components/GameScene";
@@ -5,11 +6,25 @@ import "./Screen.css";
 
 export default function Screen() {
 
+  const shot = useRef(null)
+
+  window.addEventListener("keypress", (e) => {
+
+    if (e.key == " ") {
+      shot.current.currentTime = 0
+      shot.current.volume = 0.3
+      shot.current.play()
+    }
+  })
+
   return (
+    <>
+    <audio ref={shot} src="./src/assets/audio/laser-shot.mp3"></audio>
     <Canvas>
       <GameContextProvider>
         <GameScene />
       </GameContextProvider>
     </Canvas>
+    </>
   );
-}
+} 
