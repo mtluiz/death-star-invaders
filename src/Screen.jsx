bc7228301ef7a5b983f27684e89ae7a54863a27d
Matheus Madureira <matheusluizn@gmail.com>
2022-04-16 00:00:34 -0300

    feat: creating start menu

--- src/Screen.jsx
+++ src/Screen.jsx
@@ -1,31 +1,43 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { GameContextProvider, useGameContext } from "./context/gameContext";
 import { Canvas } from "@react-three/fiber";
 import GameScene from "./components/GameScene";
 import "./Screen.css";
-import "./Screen"
+import "./Screen";
+
+import StartMenu from "./components/StartMenu";
 
 export default function Screen() {
+  const [gameBegin, setGameBegin] = useState(false);
 
-  const shot = useRef(null)
+  const shot = useRef(null);
 
   window.addEventListener("keypress", (e) => {
-
     if (e.key == " ") {
-      shot.current.currentTime = 0
-      shot.current.volume = 0.3
-      shot.current.play()
+      shot.current.currentTime = 0;
+      shot.current.volume = 0.3;
+      shot.current.play();
     }
-  })
+  });
 
   return (
     <>
-    <audio ref={shot} src="./src/assets/audio/laser-shot.mp3"></audio>
-    <Canvas>
-      <GameContextProvider>
-        <GameScene />
-      </GameContextProvider>
-    </Canvas>
+      <audio ref={shot} src="./src/assets/audio/laser-shot.mp3"></audio>
+
+      {gameBegin ? (
+        <Canvas>
+          <GameContextProvider>
+            <GameScene />
+          </GameContextProvider>
+        </Canvas>
+      ) : (
+        <StartMenu begin={()=>{ setGameBegin(true) }} />
+      )}
     </>
   );
-} 
+}
+
+/*
+
+
+  */
